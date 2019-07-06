6b790869ad280d821702fb4d0dac75b60a7ddc44
kevgk <kevgk@pm.me>
2019-07-06 13:36:13 +0200

    decouple dropDownItem text from value and searchType

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,14 @@ const dropDownItem = document.querySelectorAll('#dropdown-menu li');
 const searchButton = document.querySelector('#search-button');
 const searchBox = document.querySelector('#search-box');
 
+let searchType = 0;
+
 document.addEventListener('DOMContentLoaded', () => {
   dropDownButton.addEventListener('click', () => dropDownMenu.style.display = 'block');
 
   dropDownItem.forEach(item => {
     item.addEventListener('click', () => {
+      searchType = item.value;
       dropDownButton.innerText = item.innerText;
       dropDownMenu.style.display = 'none';
     });
@@ -21,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function search() {
-	let type = dropDownButton.innerText;
+	// let type = dropDownButton.value;
   let query = searchBox.value;
   let commonToAll = `
     -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml)
@@ -29,19 +32,19 @@ function search() {
     intitle:\"index.of./\"
   `;
 
-  const extensions = {
-    'Video': '(avi|mkv|mov|mp4|mpg|wmv)',
-    'Audio': '(ac3|flac|m4a|mp3|ogg|wav|wma)',
-    'Ebook': '(CBZ|CBR|CHM|DOC|DOCX|EPUB|MOBI|ODT|PDF|RTF|txt)',
-    'Pictures': '(bmp|gif|jpg|png|psd|tif|tiff)',
-    'Software/Games': '(apk|exe|iso|rar|tar|zip)',
-    'Compressed': '(apk|exe|iso|rar|tar|zip|7z)'
-  }
+  const extensions = [
+    '(avi|mkv|mov|mp4|mpg|wmv)',
+    '(ac3|flac|m4a|mp3|ogg|wav|wma)',
+    '(CBZ|CBR|CHM|DOC|DOCX|EPUB|MOBI|ODT|PDF|RTF|txt)',
+    '(bmp|gif|jpg|png|psd|tif|tiff)',
+    '(apk|exe|iso|rar|tar|zip)',
+    '(apk|exe|iso|rar|tar|zip|7z)'
+  ]
 
   let searchTerms = query.split(',');
   searchTerms = searchTerms.map(term => `intext:"${term.trim()}"`).join(' ');
 
-  let finalquery = `${searchTerms} ${extensions[type]} ${commonToAll}`;
+  let finalquery = `${searchTerms} ${extensions[searchType]} ${commonToAll}`;
 
   let url = `https://www.google.com/search?q=${encodeURIComponent(finalquery)}`;
 
